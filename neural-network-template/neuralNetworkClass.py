08b182758087d8e6ddbf76ba61b2f11b6360f746
Justin Marshall <jmarsha82@yahoo.com>
2023-04-15 22:45:27 -0500

    Added linked weight matrices and act fx

--- neural-network-template/neuralNetworkClass.py
+++ neural-network-template/neuralNetworkClass.py
@@ -1,3 +1,6 @@
+import numpy
+import scipy
+
 # neural network class definition
 class NeuralNetwork:
     
@@ -8,7 +11,17 @@ class NeuralNetwork:
         self.hnodes = hiddenNodes
         self.onodes = outputNodes
         self.lr = learningRate
-        pass
+        
+        # link weight matrices, wih and who
+        # weigths inside the arrays are w_i_j, where link from node i to nod k in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+        
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+        pass    
     
     # Getters and Setters
     def get_inodes(self):
@@ -41,20 +54,22 @@ class NeuralNetwork:
     
     # query the neural network
     def query():
+        # convert inputs list to 2d array
+        
         pass
 
     pass
 
-inputNodes = 3
-hiddenNodes = 3
-outputNodes = 3
-learningRate = 0.3
+# inputNodes = 3
+# hiddenNodes = 3
+# outputNodes = 3
+# learningRate = 0.3
 
-n = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+# n = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 
-n.set_hnodes(hiddenNodes)
+# n.set_hnodes(hiddenNodes)
 
-print("n inputNodes : " + str(n.get_inodes()))
-print("n hiddenNodes : " + str(n.get_hnodes()))
-print("n outputNodes : " + str(n.get_onodes()))
-print("n learningRate : " + str(n.get_lr()))
+# print("n inputNodes : " + str(n.get_inodes()))
+# print("n hiddenNodes : " + str(n.get_hnodes()))
+# print("n outputNodes : " + str(n.get_onodes()))
+# print("n learningRate : " + str(n.get_lr()))
