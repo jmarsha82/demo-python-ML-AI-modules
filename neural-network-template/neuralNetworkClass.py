c89228603c839906be850d189d047c2fda1c6bdc
Justin Marshall <jmarsha82@yahoo.com>
2023-04-16 21:58:17 -0500

    Updated Train function

--- neural-network-template/neuralNetworkClass.py
+++ neural-network-template/neuralNetworkClass.py
@@ -49,23 +49,40 @@ class NeuralNetwork:
         self.lr = value
         
     # train the neural network
-    def train():
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate signals emerging from hiddle layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output
+        final_outputs = self.activation_function(final_inputs)
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden node
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))     
         pass
     
     # query the neural network
     def query(self, inputs_list):
         # convert inputs list to 2d array
         inputs = numpy.array(inputs_list, ndmin=2).T
-        
         # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate signals emerging from hiddle layer
         hidden_outputs = self.activation_function(hidden_inputs)
-        
         # calculate signals into final output layer
         final_inputs = numpy.dot(self.who, hidden_outputs)
-        
         # calculate the signals emerging from final output
         final_outputs = self.activation_function(final_inputs)
-        
         return final_outputs
 
 # inputNodes = 3
