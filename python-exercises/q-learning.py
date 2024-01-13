f974cf487b7fab7f7d2d0cf960feda06cfb59b19
Justin Marshall <jmarsha82@yahoo.com>
2024-01-13 09:05:57 -0600

    Updated initial state random seed

--- python-exercises/q-learning.py
+++ python-exercises/q-learning.py
@@ -1,9 +1,14 @@
 import gym
-import pygame
 import random
 
 random.seed(1234)
 
 streets = gym.make("Taxi-v3", render_mode="human").env #New versions keep getting released; if -v3 doesn't work, try -v2 or -v4
 streets.reset()
+# streets.render()
+
+initial_state = streets.encode(2, 3, 2, 0)
+
+streets.s = initial_state
+
 streets.render()
